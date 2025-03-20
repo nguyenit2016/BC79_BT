46bc3e5f1c29f6248123eb89065dcd8727ebb7b8
tranathi <trananhthi030501@gmail.com>
2025-03-20 21:39:09 +0700

    Cập nhật mô tả thay đổi

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -3,11 +3,25 @@ import ListMovie from './ListMovie'
 import BannerCarousel from '../../components/BannerCarousel/BannerCarousel'
 import CinemaSystem from '../../components/BannerCarousel/CinemaSystem'
 import Showtimes from '../../components/BannerCarousel/Showtime'
+import { Link } from "react-router-dom";
 
 
 export default function HomePage() {
   return (
     <div>
+      {/* Thanh điều hướng chứa nút đăng ký / đăng nhập */}
+      <div className="absolute top-4 right-4 flex gap-3">
+        <Link to="/register">
+          <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600">
+            Đăng ký
+          </button>
+        </Link>
+        <Link to="/login">
+          <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+            Đăng nhập
+          </button>
+        </Link>
+      </div>
       <BannerCarousel /><br /><br />
       <ListMovie />
       <div className="grid grid-cols-12 gap-4 p-4">
@@ -22,5 +36,5 @@ export default function HomePage() {
       </div>
     </div>
     </div>
-  )
+  );
 }
